5cca5534d2f673a3ed550b389864f853c703a255
bbake <bbakewai@gmail.com>
2019-10-08 13:47:07 +0530

    Implemented: Load more articles dynamically on Articles

--- app/src/main/java/com/projectreachout/PostFeed/FeedMainFragment.java
+++ app/src/main/java/com/projectreachout/PostFeed/FeedMainFragment.java
@@ -3,10 +3,10 @@ package com.projectreachout.PostFeed;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -14,84 +14,36 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.snackbar.Snackbar;
-import com.projectreachout.AppController;
 import com.projectreachout.R;
 import com.projectreachout.Utilities.BackgroundSyncUtilities.BackgoundServerChecker;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.projectreachout.Utilities.CallbackUtilities.OnServerRequestResponse;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
-import static com.projectreachout.GeneralStatic.JSONParsingArrayFromString;
-import static com.projectreachout.GeneralStatic.JSONParsingIntFromObject;
-import static com.projectreachout.GeneralStatic.JSONParsingObjectFromArray;
-import static com.projectreachout.GeneralStatic.JSONParsingStringFromObject;
-import static com.projectreachout.GeneralStatic.LOAD_MORE;
-import static com.projectreachout.GeneralStatic.REFRESH;
-import static com.projectreachout.GeneralStatic.getDomainUrl;
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FeedMainFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FeedMainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FeedMainFragment extends Fragment{
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
+public class FeedMainFragment extends Fragment implements OnServerRequestResponse, AbsListView.OnScrollListener {
     private static final String TAG = FeedMainFragment.class.getSimpleName();
 
-    public FeedMainFragment() {
-        // Required empty public constructor
-    }
+    private OnFragmentInteractionListener mListener;
+    public static FeedListAdapter mFeedListAdapter;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FeedMainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FeedMainFragment newInstance(String param1, String param2) {
-        FeedMainFragment fragment = new FeedMainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static List<FeedItem> mFeedItemList;
+    private LinearLayout mErrorMessageLayout;
+    private Button mRetryBtn;
+
+    private boolean hasLockedLoadMore;
+    private View mListViewProgressBarFooterView;
+    private ListView mListView;
+
+    public FeedMainFragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -105,89 +57,30 @@ public class FeedMainFragment extends Fragment{
         super.onResume();
     }
 
-    public final String LOG_TAG_FMF = FeedMainFragment.class.getSimpleName();
-
-    public static FeedListAdapter mFeedListAdapter;
-
-    public static List<FeedItem> mFeedItemList;
-    private LinearLayout mErrorMessageLayout;
-    private Button mRetryBtn;
-
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.list_view_layout, container, false);
-
         if (mListener != null) {
             mListener.onFragmentInteraction(Uri.parse(getString(R.string.title_home)));
         }
 
-        ListView listView = rootView.findViewById(R.id.lv_lvl_list_view);
+        mListView = rootView.findViewById(R.id.lv_lvl_list_view);
         mErrorMessageLayout = rootView.findViewById(R.id.ll_lvl_error_message_layout);
         mRetryBtn = rootView.findViewById(R.id.btn_nel_retry);
+        mListViewProgressBarFooterView = LayoutInflater.from(getContext()).inflate(R.layout.footer_progress_bar_layout, mListView, false);
 
         mFeedItemList = new ArrayList<>();
 
         mFeedListAdapter = new FeedListAdapter(getActivity(), mFeedItemList);
-        listView.setAdapter(mFeedListAdapter);
+        mListView.setAdapter(mFeedListAdapter);
 
-        loadData(REFRESH);
-
-        mRetryBtn.setOnClickListener(v -> loadData(REFRESH));
-
+        GetArticleHandler.loadArticles(this, GetArticleHandler.REFRESH, GetArticleHandler.REFRESH_VALUE);
+        mListView.setOnScrollListener(this);
         return rootView;
     }
 
-    private void loadData(int action) {
-        /*Uri.Builder builder = new Uri.Builder();
-        // TODO: use .authority(getString(R.string.localhost)) after having a domain name
-        builder.scheme(getString(R.string.http))
-                .encodedAuthority(getString(R.string.localhost) + ":" + getString(R.string.port_no))
-                .appendPath("get_articles")
-                .appendPath("");*/
-
-        String url = getDomainUrl() + "/get_articles/";
-
-        switch (action) {
-            case REFRESH: {
-                loadBackgroundAsyncTask(url);
-            }
-            case LOAD_MORE: {
-                // TODO: get timeStamp of the last post in the feed list
-                /*String lastPostTimeStamp = "1556604826";
-
-                builder.appendQueryParameter("before", lastPostTimeStamp);
-                loadBackgroundAsyncTask(builder.build().toString());*/
-            }
-        }
-    }
-
-    private void loadBackgroundAsyncTask(String url) {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String output) {
-                if (output != null) {
-                    if (mErrorMessageLayout.getVisibility() == View.VISIBLE) {
-                        mErrorMessageLayout.setVisibility(View.GONE);
-                    }
-                    Log.v(TAG, output);
-                    parseJsonFeed(JSONParsingArrayFromString(output));
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                displayErrorMessage();
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                return AppController.getInstance().getLoginCredentialHeader();
-            }
-        };
-
-        AppController.getInstance().addToRequestQueue(stringRequest);
+    private int getLastVisibleArticleID() {
+        return mFeedItemList.get(mFeedItemList.size() - 1).getId();
     }
 
     private void displayErrorMessage() {
@@ -198,59 +91,9 @@ public class FeedMainFragment extends Fragment{
             Snackbar.make(Objects.requireNonNull(getView()), errorMessage, Snackbar.LENGTH_INDEFINITE).setAction("Retry", new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    loadData(REFRESH);
+                    // loadData(REFRESH);
                 }
             }).show();
-        }
-    }
-
-    private void parseJsonFeed(JSONArray response) {
-        mFeedItemList.clear();
-        for (int i = response.length()-1; i >= 0; i--) {
-            JSONObject feedObj = JSONParsingObjectFromArray(response, i);
-
-            /*Uri.Builder builder = new Uri.Builder();
-            builder.scheme(getString(R.string.http))
-                    .encodedAuthority(getString(R.string.localhost) + ":" + getString(R.string.port_no));
-
-            String url = builder.build().toString();*/
-
-            String url = getDomainUrl();
-
-            int id = JSONParsingIntFromObject(feedObj, "article_id");
-            String teamName = JSONParsingStringFromObject(feedObj, "team_name");
-            String userName = JSONParsingStringFromObject(feedObj, "username");
-            String timeStamp = JSONParsingStringFromObject(feedObj, "time_stamp");
-            String profilePictureUrl = JSONParsingStringFromObject(feedObj, "profile_picture_url");
-            String imageUrl = JSONParsingStringFromObject(feedObj, "image");
-            String description = JSONParsingStringFromObject(feedObj, "desc");
-
-            Log.v(TAG, imageUrl);
-
-            FeedItem item = new FeedItem();
-
-            item.setId(id);
-            item.setTeam_name(teamName);
-            item.setUsername(userName);
-            item.setTime_stamp(timeStamp);
-
-            item.setProfile_picture_url(url + profilePictureUrl);
-            item.setImage_url(url + imageUrl);
-
-            item.setDescription(description);
-
-            String string = item.toString();
-            Log.v(LOG_TAG_FMF, string);
-
-            mFeedItemList.add(item);
-        }
-        mFeedListAdapter.notifyDataSetChanged();
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
         }
     }
 
@@ -260,8 +103,7 @@ public class FeedMainFragment extends Fragment{
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -271,18 +113,39 @@ public class FeedMainFragment extends Fragment{
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onSuccess(int responseCode, List<?> response, String msg) {
+        if (response.isEmpty()) {
+            mListView.removeFooterView(mListViewProgressBarFooterView);
+            return;
+        }
+        if (responseCode == GetArticleHandler.REFRESH) {
+            mFeedItemList.clear();
+            mFeedItemList.addAll((Collection<? extends FeedItem>) response);
+        } else if (responseCode == GetArticleHandler.LOAD_MORE) {
+            mFeedItemList.addAll((Collection<? extends FeedItem>) response);
+        }
+        mFeedListAdapter.notifyDataSetChanged();
+        mListView.removeFooterView(mListViewProgressBarFooterView);
+        hasLockedLoadMore = false;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (!hasLockedLoadMore && firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount != 0) {
+            mListView.addFooterView(mListViewProgressBarFooterView);
+            hasLockedLoadMore = true;
+            GetArticleHandler.loadArticles(this, GetArticleHandler.LOAD_MORE, getLastVisibleArticleID());
+        }
+    }
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+        // Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
